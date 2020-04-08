9ab9e4a2d9d7ad33bafb8b712edf86c80d357aff
anh <hotrunganhht1912@gmail.com>
2020-04-08 14:55:23 +0700

    useReduce

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-### npm sart
+###useReducer
+-có 2 thành phần state and dispatch => nhận vào 2 tham số _*state hiện tại*_ và _*action*_ cho phép quản lý state theo kiến trúc flux (như là redux đang dùng )
 
-learn - reactjs - hook
+-xử dụng useReducer thay cho useReducer thay cho useState khi ta có state chứa nhiều thành phần phức tạp
+
+link: https://viblo.asia/p/tim-hieu-ve-hooks-trong-react-Ljy5VzGG5ra
+
+# Không dùng useReduce
+
+<p align="center">
+  <img src="./image/not_useReduce.png" width="100%" alt="success example">
+</p>
+
+# Dùng useReduce
+
+<p align="center">
+  <img src="./image/useReduce.png" width="100%" alt="success example">
+</p>
--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
 }
 
+const App = () => {
+  const [count, setCount] = useReducer(reducer, 0);
+  return (
+    <div>
+      <div>count: {count} </div>
+      <button onClick={() => setCount({ type: "increment" })}>increment</button>
+      <button onClick={() => setCount({ type: "decrement" })}>decrement</button>
+    </div>
+  );
+};
 export default App;
