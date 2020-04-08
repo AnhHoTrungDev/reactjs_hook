755cba4655ee715280e9cf17166425445bc22c6d
anh <hotrunganhht1912@gmail.com>
2020-04-08 17:07:13 +0700

    ex_2 useReducer

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### useReducer
--có 2 thành phần state and dispatch => nhận vào 2 tham số _*state hiện tại*_ và _*action*_ cho phép quản lý state theo kiến trúc flux (như là redux đang dùng )
+
+-có 2 thành phần state and dispatch => nhận vào 2 tham số _*state hiện tại*_ và _*action*_ cho phép quản lý state theo kiến trúc **_flux_** (như là redux đang dùng )
 
 -xử dụng useReducer thay cho useReducer thay cho useState khi ta có state chứa nhiều thành phần phức tạp
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
-function reducer(state, action) {
+function reducerTodo(state, action) {
   switch (action.type) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
+    case "add-todo":
+      console.log("state :", state);
+      console.log("action :", action);
+      console.log("=>");
+      return {
+        todos: [...state.todos, { text: action.text, isCompleted: false }],
+      };
+    case "toggle-todo":
+      return {
+        todos: state.todos.map((todo, index) =>
+          index === action.idx
+            ? { ...todo, isCompleted: !todo.isCompleted }
+            : todo
+        ),
+      };
     default:
       return state;
   }
 }
 
 const App = () => {
-  const [count, setCount] = useReducer(reducer, 0);
+  //`      #state    #dispatch               #function   #gía trị ban đầu (initialState)`;
+  const [{ todos }, handleTodo] = useReducer(reducerTodo, { todos: [] });
+  const [text, setText] = useState("");
+
   return (
     <div>
-      <div>count: {count} </div>
-      <button onClick={() => setCount({ type: "increment" })}>increment</button>
-      <button onClick={() => setCount({ type: "decrement" })}>decrement</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleTodo({ type: "add-todo", text });
+          setText("");
+        }}
+      >
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </form>
+
+      {todos.map((todo, index) => (
+        <div
+          key={index}
+          onClick={() => handleTodo({ type: "toggle-todo", idx: index })}
+          style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+        >
+          {todo.text}
+        </div>
+      ))}
+
+      <pre>{JSON.stringify(todos, null, 2)}</pre>
     </div>
   );
 };
